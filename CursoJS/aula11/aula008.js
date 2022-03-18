2e6a711c03351ea8c6c3fdec797a41dcffd774cf
Leonardo Vaz <87207693+leowvazd@users.noreply.github.com>
2022-03-18 12:31:23 -0300

    up

---
+++ CursoJS/aula11/aula008.js
@@ -0,0 +1,9 @@
+var vel = 12
+
+console.log(`A velocidade do seu carro é ${vel} km por hora`)
+
+if(vel > 60) {
+  console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+
+console.log('Dirija sempre usando cinto de segurança')
