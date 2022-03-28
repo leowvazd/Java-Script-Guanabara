6a699c49661d8be8f40c852baa89ad5c2cd764a0
Leonardo Vaz <87207693+leowvazd@users.noreply.github.com>
2022-03-28 11:15:53 -0300

    ex. 15
    update: done ex. 015

---
+++ CursoJS/aula12ex/ex015/script.js
@@ -0,0 +1,60 @@
+function verificar() {
+
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        // window.alert('Tudo ok!')
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        // res.innerHTML = `Idade calculada: ${idade}`
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+            gênero = 'homem'
+
+            if (idade >= 0 && idade < 10) {
+                // criança
+                img.setAttribute('src', 'crianca1.jpg')
+            } else if (idade < 21) {
+                // jovem
+                img.setAttribute('src', 'jovem1.jpg')
+            } else if (idade < 50) {
+                // adulto
+                img.setAttribute('src', 'adulto1.jpg')
+            } else {
+                //idoso
+                img.setAttribute('src', 'velho1.jpg')
+            }
+
+        } else if (fsex[1].checked) {
+            gênero = 'mulher'
+
+            if (idade >= 0 && idade < 10) {
+                // criança
+                img.setAttribute('src', 'crianca2.jpg')
+            } else if (idade < 21) {
+                // jovem
+                img.setAttribute('src', 'jovem2.jpg')
+            } else if (idade < 50) {
+                // adulto
+                img.setAttribute('src', 'adulto2.jpg')
+            } else {
+                //idoso
+                img.setAttribute('src', 'velho2.jpg')
+            }
+        }
+
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos!`
+        
+        res.appendChild(img)
+    }
+
+}
