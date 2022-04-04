1915ac6587edb816ab68c22347d556da486a8f68
Leonardo Vaz <87207693+leowvazd@users.noreply.github.com>
2022-04-04 09:57:24 -0300

    up
    up

--- CursoJS/aula14ex/ex016/script.js
+++ CursoJS/aula14ex/ex016/script.js
@@ -1,1 +1,36 @@
-alert('Olá! :)')
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível Contar! :('
+        // window.alert('[ERRO] Faltam Dados!')        
+    } else {
+        // alert ('Tudo Ok!')
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        
+        if (p <= 0) {
+            window.alert("Passo inválido! Considerando Passo Igual a 1! :P")
+            p = 1
+        }
+
+        if (i < f) {
+            // Contagem crescente
+            for (let c = i; c <= f; c+=p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+            // Contagem regreessiva
+        } else {
+            for (let c = i; c>= f; c-= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+
+        res.innerHTML += ` \u{1F3C1}`
+    }
+}
