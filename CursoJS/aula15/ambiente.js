720403f19126f9e5f64bf87723d823604fd712aa
Leonardo Vaz <87207693+leowvazd@users.noreply.github.com>
2022-04-13 17:43:02 -0300

    up
    up

--- CursoJS/aula15/ambiente.js
+++ CursoJS/aula15/ambiente.js
@@ -1,3 +1,22 @@
-let num = []
+let num = [5, 8, 4]
 
-console.log()
+num[3] = 6
+
+num.push(7)
+
+num[num.length] = 10
+
+num.sort()
+
+// console.log(num)
+
+// console.log(`\nNosso vetor é o: ${num}\n`)
+
+// console.log(`O vetor tem ${num.length} posições\n`)
+
+for (var i = 0; i < num.length; i++) {
+    console.log(`O valor do vetor na posição ${i} é ${num[i]}`)
+}
+
+let pos = num.indexOf(11)
+console.log(pos)
